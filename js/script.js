3a0a0746ec5e5db9e3056c034ec2fa4a31ce3afe
Sophia Angelina <161032086+sophiaasb@users.noreply.github.com>
2024-10-08 19:05:37 +0000

    ok

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//dom
+const distancia = document.querySelector('#distancia')
+const consumo = document.querySelector('#consumo')
+const preco = document.querySelector('#preco')
+const botao = document.querySelector('#botao')
+const resultado = document.querySelector('#resultado')
+
+//evento
+botao.addEventListener('click', combustivel)
+
+//função
+function combustivel(){
+    d= Number(distancia.value)
+    c= Number(consumo.value)
+    p= Number(preco.value)
+    valorfinal = (d/c)*p
+    resultado.textContent = 
+}
